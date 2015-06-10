ccda66bc5e0ac629ed2377548af8e9022d39ae11
Jon Borgonia <jon@gomagames.com>
2015-06-09 14:35:11 -1000

    fixes test result for sumOfHighInterests

--- test/array-methods-spec.js
+++ test/array-methods-spec.js
@@ -67,7 +67,7 @@ describe('Array Methods', function() {
   describe('sumOfHighInterests', function() {
 
     it('should be the sum the 18.9% interest for all amounts in bankBalances, where the amount of the sum of interests in that state is greater than 50,000, in the selected states, rounded to the nearest cent', function() {
-      arrayMethods.sumOfHighInterests.should.be.equal(7985380.78);
+      arrayMethods.sumOfHighInterests.should.be.equal(7935913.99);
     });
 
   });
