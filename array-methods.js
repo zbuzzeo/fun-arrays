58abe0e5755c3df58ebb6db469894ad4553a127a
sgnl <rayrfarias@gmail.com>
2017-04-01 19:47:50 -1000

    📝 updates copy

--- array-methods.js
+++ array-methods.js
@@ -2,15 +2,19 @@ var dataset = require('./dataset.json');
 
 /*
   create an array with accounts from bankBalances that are
-  greater than 100000.00
+  greater than 100000
   assign the resulting array to `hundredThousandairs`
 */
 var hundredThousandairs = null;
 
 /*
-  set a new key for each object in bankBalances named `rounded`
-  the value of this key will be the `amount` rounded to the nearest dollar
-  example 
+  Create a new object, DO NOT MUTATE DATA.
+
+  The new object will have all the same properties e.g. `state` and `amount`
+  You will add one new key to this object: `rounded`
+  The value of this key will be the `amount` rounded to the nearest dollar
+
+  Example:
     {
       "amount": "134758.44",
       "state": "HI",
@@ -21,103 +25,111 @@ var hundredThousandairs = null;
 var roundedDollar = null;
 
 /*
-  set a the `amount` value for each object in bankBalances
-  to the value of `amount` rounded to the nearest 10 cents
-  example 
+  Create a new object, DO NOT MUTATE DATA.
+
+  The new object will have all the same properties e.g. `state` and `amount`
+  You will add one new key to this object: `roundedDime`
+  The value of this key will be the `amount` rounded to the nearest 10th of a cent
+
+  Example
     {
-      "amount": 134758.4,
+      "amount": "134758.46",
       "state": "HI"
+      "roundedDime": 134758.5
     }
   assign the resulting array to `roundedDime`
 */
 var roundedDime = null;
 
-// set sumOfBankBalances to the sum of all amounts in bankBalances
+// set sumOfBankBalances to be the sum of all value held at `amount` for each bank object
 var sumOfBankBalances = null;
 
 /*
-  set sumOfInterests to the sum of the 18.9% interest
-  for all amounts in bankBalances
-  in each of the following states
+  from each of the following states:
     Wisconsin
     Illinois
     Wyoming
     Ohio
     Georgia
     Delaware
-  the result should be rounded to the nearest cent
+  take each `amount` and add 18.9% interest to it rounded to the nearest cent
+  and then sum it all up into one value saved to `sumOfInterests`
  */
 var sumOfInterests = null;
 
 /*
-  set sumOfHighInterests to the sum of the 18.9% interest
-  for all amounts in bankBalances
-  where the amount of the sum of interests in that state is
-    greater than 50,000
-  in every state except
+  from each of the following states:
     Wisconsin
     Illinois
     Wyoming
     Ohio
     Georgia
     Delaware
-  the result should be rounded to the nearest cent
+    find the banks where the `amount` is greater than 50,000
+    add 18.9% interest to that `amount`
+    and then sum up the `amount` values for each bank
+    and round it to the nearest cent
+
+    the result should be saved to `sumOfHighInterests`
  */
 var sumOfHighInterests = null;
 
 /*
   aggregate the sum of bankBalance amounts
   grouped by state
-  set stateSums to be a hash table
-    where the key is the two letter state abbreviation
-    and the value is the sum of all amounts from that state
-      the value must be rounded to the nearest cent
+  set stateSums to be a hash table where
+
+  the key is:
+    the two letter state abbreviation
+  and the value is:
+    the sum of all amounts from that state
+    the value must be rounded to the nearest cent
  */
 var stateSums = null;
 
 /*
-  set lowerSumStates to an array containing
-  only the two letter state abbreviation of each state 
-  where the sum of amounts in the state is
-    less than 1,000,000
+  set `lowerSumStates` to be an array of two letter state
+  abbreviations of each state where the sum of amounts
+  in the state is less than 1,000,000
  */
 var lowerSumStates = null;
 
 /*
-  set higherStateSums to be the sum of 
-    all amounts of every state
-    where the sum of amounts in the state is
-      greater than 1,000,000
+  aggregate the sum of each state into one hash table
+  `higherStateSums` should be the sum of all states with totals greater than 1,000,000
  */
 var higherStateSums = null;
 
 /*
-  set areStatesInHigherStateSum to be true if
-    all of these states have a sum of account values
-      greater than 2,550,000
+  from each of the following states:
     Wisconsin
     Illinois
     Wyoming
     Ohio
     Georgia
     Delaware
-  false otherwise
+
+  Check if all of these states have a sum of account values
+  greater than 2,550,000
+
+  if true set `areStatesInHigherStateSum` to `true`
+  otherwise set it to `false`
  */
 var areStatesInHigherStateSum = null;
 
 /*
   Stretch Goal && Final Boss
-  
-  set anyStatesInHigherStateSum to be true if
-    any of these states have a sum of account values
-      greater than 2,550,000
+
+  set `anyStatesInHigherStateSum` to be `true` if
+  any of these states:
     Wisconsin
     Illinois
     Wyoming
     Ohio
     Georgia
     Delaware
-  false otherwise
+  have a sum of account values greater than 2,550,000
+  otherwise set it to be `false`
  */
 var anyStatesInHigherStateSum = null;
 
